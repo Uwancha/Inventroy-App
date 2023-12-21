cbb7e4cde13387fc5531e9a76dd1d4d7e56527c0
Uwancha <sito2alemu@gmail.com>
2023-12-21 20:47:38 +0300

    feat/refactor: Refactor categoryController Remove dummy content from some methods Fetch and display data for category lists and category details

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,42 +1,66 @@
 const Category = require("../models/category");
+const Item = require("../models/item")
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Categories.
 exports.category_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED YET: Category list");
+  const categoryList = await Category.find().sort({name:1}).exec();
+  console.log(categoryList)
+
+  res.render('index', {
+    title: "Products",
+    categoryList: categoryList,
+  });
 });
 
 // Display detail page for a specific Category.
 exports.category_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED YET: Category detail: ${req.params.id}`);
+  const [category, items] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({category: req.params.id})
+  ])
+
+  if (category == null) {
+    // No results.
+    const err = new Error("Category not found");
+    err.status = 404;
+
+    return next(err);
+  }
+
+  res.render('categoryDetail', {
+    category: category,
+    items: items
+  })
+  
 });
 
 // Display Category create form on GET.
-exports.author_create_get = asyncHandler(async (req, res, next) => {
+exports.category_create_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED YET: Category create GET");
 });
 
 // Handle Category create on POST.
-exports.author_create_post = asyncHandler(async (req, res, next) => {
+exports.category_create_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED YET: Category create POST");
 });
 
 // Display Category delete form on GET.
-exports.author_delete_get = asyncHandler(async (req, res, next) => {
+exports.category_delete_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED YET: Category delete GET");
 });
 
 // Handle Category delete on POST.
-exports.author_delete_post = asyncHandler(async (req, res, next) => {
+exports.category_delete_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED YET: Category delete POST");
 });
 
 // Display Category update form on GET.
-exports.author_update_get = asyncHandler(async (req, res, next) => {
+exports.category_update_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED YET: Category update GET");
 });
 
 // Handle Category update on POST.
-exports.author_update_post = asyncHandler(async (req, res, next) => {
+exports.category_update_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED YET: Category update POST");
 });
