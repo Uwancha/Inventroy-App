d84bd5b2f707cfe574c20196f88f638388373e9e
Uwancha <sito2alemu@gmail.com>
2023-12-23 02:19:32 +0300

    refactor: Refactor Item model to adjust character limit of item description

--- models/item.js
+++ models/item.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 
 const ItemSchema = new Schema({
     name: {type: String, required: true},
-    description: {type: String, required: true, maxLength: 100},
+    description: {type: String, required: true, maxLength: 200},
     price: {type: Number, required: true},
     category : {type: Schema.Types.ObjectId, ref: "Category", required: true}
 })
