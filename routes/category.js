791de9f8561d502646a4c000f58754ae6ba2ee63
Uwancha <sito2alemu@gmail.com>
2023-12-22 21:23:00 +0300

    feat: Set up routes for creating, updating and deleting a category

--- routes/category.js
+++ routes/category.js
@@ -4,8 +4,29 @@ const router = express.Router();
 // Controllers
 const categoryController = require('../controllers/categoryController')
 
-/* GET home page. */
+/* GET home page // List of all categories. */
 router.get('/category/', categoryController.category_list);
+
+// Get request for creating a category
+router.get('/category/create', categoryController.category_create_get)
+
+// Post request for creating category
+router.post('/category/create', categoryController.category_create_post)
+
+// Get detail of a category
 router.get('/category/:id', categoryController.category_detail)
+
+// Get request to delete a category
+router.get('/category/:id/delete', categoryController.category_delete_get)
+
+// Get request to delete a category
+router.post('/category/:id/delete', categoryController.category_delete_post)
+
+// Get request to delete a category
+router.get('/category/:id/update', categoryController.category_update_get)
+
+// Get request to delete a category
+router.post('/category/:id/update', categoryController.category_update_post)
+
 
 module.exports = router;
