078f645332fbe1b07849cc6282254878827fef20
Uwancha <sito2alemu@gmail.com>
2023-12-23 17:14:32 +0300

    fix: Fix typo error in items route

--- routes/item.js
+++ routes/item.js
@@ -10,20 +10,17 @@ router.get('/category/item/create/:id', itemController.item_create_get);
 // Post request to create an item
 router.post('/category/item/create/:id', itemController.item_create_post);
 
-
 // Get request to delete an item
 router.get('/category/item/delete/:categoryid/:id', itemController.item_delete_get);
 
 // Get request to delete an item
 router.post('/category/item/delete/:categoryid/:id', itemController.item_delete_post);
 
-
-
 // Get request to update an item
 router.get('/category/item/update/:categoryid/:itemid', itemController.item_update_get);
 
 // Post request to update an item
-router.post('/category/item/update/:categoryid/:id', itemController.item_update_post);
+router.post('/category/item/update/:categoryid/:itemid', itemController.item_update_post);
 
 
 module.exports = router;
