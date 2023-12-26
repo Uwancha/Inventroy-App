c4e3a0d59182bf81daf76278fcb3b42503cbeb90
Uwancha <sito2alemu@gmail.com>
2023-12-26 15:41:14 +0300

    feat: Protect admin routes Implemnted admin feature. That's only admins can delete and update an item or a category

--- routes/category.js
+++ routes/category.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require("../authmiddleware").isAuth;
+const isAdmin = require("../authmiddleware").isAdmin;
+
 
 // Controllers
 const categoryController = require('../controllers/categoryController')
@@ -17,13 +20,13 @@ router.post('/category/create', categoryController.category_create_post)
 router.get('/category/:id', categoryController.category_detail)
 
 // Get request to delete a category
-router.get('/category/:id/delete', categoryController.category_delete_get)
+router.get('/category/:id/delete', isAuth, isAdmin, categoryController.category_delete_get)
 
 // Get request to delete a category
 router.post('/category/:id/delete', categoryController.category_delete_post)
 
 // Get request to delete a category
-router.get('/category/:id/update', categoryController.category_update_get)
+router.get('/category/:id/update', isAuth, isAdmin, categoryController.category_update_get)
 
 // Get request to delete a category
 router.post('/category/:id/update', categoryController.category_update_post)
--- routes/item.js
+++ routes/item.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const isAuth = require("../authmiddleware").isAuth;
+const isAdmin = require("../authmiddleware").isAdmin;
+
+
 // Controllers
 const itemController = require('../controllers/itemController')
 
@@ -11,13 +15,13 @@ router.get('/category/item/create/:id', itemController.item_create_get);
 router.post('/category/item/create/:id', itemController.item_create_post);
 
 // Get request to delete an item
-router.get('/category/item/delete/:categoryid/:id', itemController.item_delete_get);
+router.get('/category/item/delete/:categoryid/:id', isAuth, isAdmin, itemController.item_delete_get);
 
 // Get request to delete an item
 router.post('/category/item/delete/:categoryid/:id', itemController.item_delete_post);
 
 // Get request to update an item
-router.get('/category/item/update/:categoryid/:itemid', itemController.item_update_get);
+router.get('/category/item/update/:categoryid/:itemid', isAuth, isAdmin, itemController.item_update_get);
 
 // Post request to update an item
 router.post('/category/item/update/:categoryid/:itemid', itemController.item_update_post);
